d3e950deed2627e715542ddb862534eefb4fffa7
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-06-08 17:20:04 +0100

    dynamic calculat body podding depend navbar

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+  // dynamic calculat body podding depend navbar
+  $("body").css("paddingTop", $(".navbar").innerHeight()+10);
+  $(window).on("resize", function () {
+    $("body").css("paddingTop", $(".navbar").innerHeight()+10);
+  });
+
   // smooth scroll
   $(".navbar li a").click(function (e) {
     e.preventDefault();
