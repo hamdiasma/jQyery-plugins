dc67c092decadcb53e8bc42940e74112b76e7d11
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-06-08 17:44:43 +0100

    add and remove class active to links and remove all from siblings

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   // dynamic calculat body podding depend navbar
-  $("body").css("paddingTop", $(".navbar").innerHeight()+10);
+  $("body").css("paddingTop", $(".navbar").innerHeight() + 10);
   $(window).on("resize", function () {
-    $("body").css("paddingTop", $(".navbar").innerHeight()+10);
+    $("body").css("paddingTop", $(".navbar").innerHeight() + 10);
   });
 
   // smooth scroll
@@ -14,5 +14,16 @@ $(document).ready(function () {
       },
       1000
     );
+  });
+  // add and remove class active to links and remove all from siblings
+  $(".navbar li a").click(function () {
+    // $(this)
+    //   .addClass("active")
+    //   .parents()
+    //   .siblings()
+    //   .find("a")
+    //   .removeClass("active");
+    $(".navbar li a").removeClass("active");
+    $(this).addClass("active");
   });
 });
