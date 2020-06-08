71a9c5f5fbc1ef3f5b596993ca84bae3d38bbaf6
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-06-08 21:53:26 +0100

    think navbar with sections

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   // dynamic calculat body podding depend navbar
-  $("body").css("paddingTop", $(".navbar").innerHeight() + 10);
+  $("body").css("paddingTop", $(".navbar").innerHeight());
   $(window).on("resize", function () {
-    $("body").css("paddingTop", $(".navbar").innerHeight() + 10);
+    $("body").css("paddingTop", $(".navbar").innerHeight());
   });
 
   // smooth scroll
@@ -10,7 +10,7 @@ $(document).ready(function () {
     e.preventDefault();
     $("html,body").animate(
       {
-        scrollTop: $(`#${$(this).data("scroll")}`).offset().top,
+        scrollTop: $(`#${$(this).data("scroll")}`).offset().top+1,
       },
       1000
     );
@@ -25,5 +25,26 @@ $(document).ready(function () {
     //   .removeClass("active");
     $(".navbar li a").removeClass("active");
     $(this).addClass("active");
+  });
+
+  // think navbar with sections
+  $(window).scroll(function () {
+    // $(".div").each(function () {
+    //   if ($(window).scrollTop() > $(this).offset().top) {
+    //     console.log($(this).attr("id"));
+    //     var divId = $(this).attr("id");
+    //     $(".navbar li a").removeClass("active");
+    //     $(`.navbar li a[data-scroll=${divId}]`).addClass("active");
+    //   }
+    // });
+
+    $(".div").each(function () {
+      if ($(window).scrollTop() > $(this).offset().top) {
+        // console.log($(this).attr("id"));
+        var div = $(this).attr("id");
+        $(".navbar li a").removeClass("active");
+        $(".navbar li a[data-scroll='" + div + "']").addClass("active");
+      }
+    });
   });
 });
