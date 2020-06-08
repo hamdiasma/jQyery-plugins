dcb111560a16d9469646e39ea802ed7ac7ebd175
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-06-08 16:34:16 +0100

    smoothly scroll

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  // smooth scroll
+  $(".navbar li a").click(function (e) {
+    e.preventDefault();
+    $("html,body").animate(
+      {
+        scrollTop: $(`#${$(this).data("scroll")}`).offset().top,
+      },
+      1000
+    );
+  });
+});
